1d92faa954d8b69711c1f46d7a5c4020b45b6fa1
Travis Luong <travis@travisluong.com>
2013-06-25 22:57:15 -0700

    activate static_blocks authentication

--- config/initializers/static_block.rb
+++ config/initializers/static_block.rb
@@ -1,6 +1,6 @@
 StaticBlocks.config do |config|
   config.locales = ['en']
-  config.http_auth = false
+  config.http_auth = true
   config.username = ENV['STATIC_BLOCKS_USERNAME']
   config.password = ENV['STATIC_BLOCKS_PASSWORD']
 end
